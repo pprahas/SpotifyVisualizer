12b7aa7be0840dd22a08c0d82b7811b79905cc7d
prado156 <ppattem@purdue.edu>
2022-10-05 13:56:03 -0400

    added client env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
+const querystring = require("query-string");
 
 require("dotenv").config();
 const port = 8888;
@@ -14,6 +15,40 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
+});
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+const generateRandomString = (length) => {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const stateKey = "spotify_auth_state";
+
+app.get("/login", (req, res) => {
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  const scope = "user-read-private user-read-email";
+
+  const queryParams = querystring.stringify({
+    client_id: "69cb2875d7a843548d9e9afc44188876",
+    response_type: "code",
+    redirect_uri: "http://localhost:8888/callback",
+    state: state,
+    scope: scope,
+  });
+
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
 
 console.log(process.env.CLIENT_ID);
